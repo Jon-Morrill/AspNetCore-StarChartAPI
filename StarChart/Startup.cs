346fcb3846829ca6113045a3d8c7d3195d6b50cf
Jon Morrill <jon.r.morrill@emcins.com>
2020-07-13 11:57:57 -0500

    picky system

--- StarChart/Startup.cs
+++ StarChart/Startup.cs
@@ -13,8 +13,8 @@ namespace StarChart
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
             services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("StarChart"));
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
